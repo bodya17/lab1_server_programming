7e1680644402fb4854b6ec2545260a25461ac25f
bodya17 <levantovychbogdan@gmail.com>
2017-10-23 22:49:39 +0300

    add bridge table

--- Interface/Form1.Designer.cs
+++ Interface/Form1.Designer.cs
@@ -30,17 +30,25 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.book_data_grid = new System.Windows.Forms.DataGridView();
             this.button2 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.author_data_grid = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.author_name_text_box = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.save_author_to_db_button = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.book_data_grid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.author_data_grid)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(378, 48);
+            this.button1.Location = new System.Drawing.Point(305, 48);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -50,22 +58,15 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(130, 41);
+            this.textBox1.Location = new System.Drawing.Point(130, 51);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 1;
             // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(130, 80);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 2;
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(79, 48);
+            this.label1.Location = new System.Drawing.Point(79, 54);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(27, 13);
             this.label1.TabIndex = 3;
@@ -74,25 +75,25 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(79, 87);
+            this.label2.Location = new System.Drawing.Point(79, 103);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(38, 13);
             this.label2.TabIndex = 4;
             this.label2.Text = "Author";
             // 
-            // dataGridView1
+            // book_data_grid
             // 
-            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(58, 117);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(499, 150);
-            this.dataGridView1.TabIndex = 5;
-            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.updateData);
+            this.book_data_grid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.book_data_grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.book_data_grid.Location = new System.Drawing.Point(68, 151);
+            this.book_data_grid.Name = "book_data_grid";
+            this.book_data_grid.Size = new System.Drawing.Size(436, 150);
+            this.book_data_grid.TabIndex = 5;
+            this.book_data_grid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.updateData);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(378, 82);
+            this.button2.Location = new System.Drawing.Point(305, 102);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(105, 23);
             this.button2.TabIndex = 6;
@@ -100,22 +101,101 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.deleteFromDb);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 18.215F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(234, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 28);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Books";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 18.215F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(688, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 28);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Authors";
+            // 
+            // author_data_grid
+            // 
+            this.author_data_grid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.author_data_grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.author_data_grid.Location = new System.Drawing.Point(542, 151);
+            this.author_data_grid.Name = "author_data_grid";
+            this.author_data_grid.Size = new System.Drawing.Size(436, 150);
+            this.author_data_grid.TabIndex = 9;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(130, 102);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 10;
+            // 
+            // author_name_text_box
+            // 
+            this.author_name_text_box.Location = new System.Drawing.Point(663, 57);
+            this.author_name_text_box.Name = "author_name_text_box";
+            this.author_name_text_box.Size = new System.Drawing.Size(100, 20);
+            this.author_name_text_box.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(581, 60);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Author name";
+            // 
+            // save_author_to_db_button
+            // 
+            this.save_author_to_db_button.Location = new System.Drawing.Point(814, 55);
+            this.save_author_to_db_button.Name = "save_author_to_db_button";
+            this.save_author_to_db_button.Size = new System.Drawing.Size(75, 23);
+            this.save_author_to_db_button.TabIndex = 13;
+            this.save_author_to_db_button.Text = "Save to DB";
+            this.save_author_to_db_button.UseVisualStyleBackColor = true;
+            this.save_author_to_db_button.Click += new System.EventHandler(this.saveAuthorToDB);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(416, 48);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 14;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(618, 393);
+            this.ClientSize = new System.Drawing.Size(1000, 393);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.save_author_to_db_button);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.author_name_text_box);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.author_data_grid);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.book_data_grid);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.book_data_grid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.author_data_grid)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -125,11 +205,18 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button2;
-        public System.Windows.Forms.DataGridView dataGridView1;
+        public System.Windows.Forms.DataGridView book_data_grid;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        public System.Windows.Forms.DataGridView author_data_grid;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox author_name_text_box;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button save_author_to_db_button;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- Interface/Form1.cs
+++ Interface/Form1.cs
@@ -18,15 +18,7 @@ namespace Interface
             InitializeComponent();
             this.isIDAtZero = true;
             this.renderUpdateButtons = true;
-            //using (LibraryContext context = new LibraryContext())
-            //{
-            //    Author author = new Author();
-            //    author.Name = "Vasya";
 
-            //    context.Authors.Add(author);
-            //    //context.Author
-            //    context.SaveChanges();
-            //}
         }
 
         private bool isIDAtZero;
@@ -36,12 +28,20 @@ namespace Interface
         {
             using (LibraryContext context = new LibraryContext())
             {
-                dataGridView1.DataSource = context.Books.ToList();
+                var bookList = context.Books.Select(x => new { BookId = x.BookId, Title = x.Title }).ToList();
+                var authorList = context.Authors.Select(x => new { AuthorId = x.AuthorId, Name = x.Name}).ToList();
+
+                book_data_grid.DataSource = bookList;
+                author_data_grid.DataSource = authorList;
+
+                this.comboBox1.DataSource = context.Authors.Select(x => x.Name).ToList();
+                
+
             }
 
             if (this.renderUpdateButtons)
             {
-                dataGridView1.Columns[0].ReadOnly = true; // disable id editing
+                //dataGridView1.Columns[0].ReadOnly = true; // disable id editing
 
                 var buttonCol = new DataGridViewButtonColumn();
                 buttonCol.Name = "Update";
@@ -49,18 +49,18 @@ namespace Interface
                 buttonCol.Text = "Update";
                 buttonCol.UseColumnTextForButtonValue = true;
 
-                dataGridView1.Columns.Insert(3, buttonCol);
+                book_data_grid.Columns.Insert(2, buttonCol);
                 this.renderUpdateButtons = false;
             } else
             {
-                dataGridView1.Columns[1].ReadOnly = true; // disable id editing
+                //dataGridView1.Columns[1].ReadOnly = true; // disable id editing
                 isIDAtZero = false;
             }
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //this.Display();
+            this.Display();
         }
 
         private void createNewBookInDb(object sender, EventArgs e)
@@ -70,7 +70,10 @@ namespace Interface
                 Book book = new Book();
                 book.Title = this.textBox1.Text;
                 //book.Author = this.textBox2.Text;
-
+                Console.WriteLine(comboBox1.Text);
+                Author author = context.Authors.Where(a => a.Name == comboBox1.Text).Single();
+                author.Books.Add(book);
+               //book.Authors.Add(author);
                 context.Books.Add(book);
                 context.SaveChanges();
 
@@ -81,9 +84,9 @@ namespace Interface
         // Delete button pressed
         private void deleteFromDb(object sender, EventArgs e)
         {
-            if (dataGridView1.Rows.Count > 0)
+            if (book_data_grid.Rows.Count > 0)
             {
-                foreach (DataGridViewRow row in dataGridView1.SelectedRows) // foreach datagridview selected rows values  
+                foreach (DataGridViewRow row in book_data_grid.SelectedRows) // foreach datagridview selected rows values  
                 {
 
                     int id;
@@ -119,16 +122,15 @@ namespace Interface
                 int id;
                 if (this.isIDAtZero)
                 {
-                    id = Convert.ToInt32(this.dataGridView1.Rows[e.RowIndex].Cells[0].Value);
+                    id = Convert.ToInt32(this.book_data_grid.Rows[e.RowIndex].Cells[0].Value);
                 }
                 else
                 {
-                    id = Convert.ToInt32(this.dataGridView1.Rows[e.RowIndex].Cells[1].Value);
+                    id = Convert.ToInt32(this.book_data_grid.Rows[e.RowIndex].Cells[1].Value);
                 }
-                var BookTitle = this.dataGridView1.Rows[e.RowIndex].Cells[i + 1].Value.ToString();
-                var AuthorName = this.dataGridView1.Rows[e.RowIndex].Cells[i + 2].Value.ToString();
+                var BookTitle = this.book_data_grid.Rows[e.RowIndex].Cells[i + 1].Value.ToString();
+                //var AuthorName = this.dataGridView1.Rows[e.RowIndex].Cells[i + 2].Value.ToString();
                 
-
                 using (LibraryContext context = new LibraryContext())
                 {
                     Book bookToUpdate = context.Books.Where(x => x.BookId == id).Select(x => x).FirstOrDefault();
@@ -139,6 +141,21 @@ namespace Interface
                 }
 
             }
+
+        }
+
+        private void saveAuthorToDB(object sender, EventArgs e)
+        {
+            var authorName = this.author_name_text_box.Text;
+            using (LibraryContext context = new LibraryContext())
+            {
+                Author author = new Author();
+                author.Name = this.author_name_text_box.Text;
+             
+                context.Authors.Add(author);
+                context.SaveChanges();
+            }
+            this.Display();
 
         }
     }
